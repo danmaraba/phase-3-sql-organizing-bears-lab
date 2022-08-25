714c52ba5c85b9bc7a09a171db58eb1c5f9d4c68
Daniel Kipruto Maraba <danielkmaraba@gmail.com>
2022-08-25 10:01:15 +0300

    commit tree 173fa925a5538b69ba71a9497ec448c00deab0e1

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "black", "hot-tealepered", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "M", "brown", "hot-tempered", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "black", "hot-tempered", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 19, "M", "black", "hot-tempered", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 13, "F", "white", "hot-tempered", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "M", "black", "hot-tempered", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, "F", "brown", "hot-tempered", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 20, "M", "black", "hot-tempered", 0);
+
